2bae0cea5102c309ddb53e230f4c27b2e9d7a497
Atos-Giandoso <atosgiandoso@gmail.com>
2021-08-22 17:20:39 -0300

    labefy

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -7,17 +7,17 @@ class App extends React.Component {
     return (
       <div className={'app-container'}>
         <Post
-          nomeUsuario={'paulinha'}
+          nomeUsuario={'Atos'}
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
         />
         <Post
-          nomeUsuario={'joao'}
+          nomeUsuario={'Naomy'}
           fotoUsuario={'https://picsum.photos/50/51'}
           fotoPost={'https://picsum.photos/200/151'}
         />
         <Post
-          nomeUsuario={'amanda'}
+          nomeUsuario={'Joan'}
           fotoUsuario={'https://picsum.photos/50/52'}
           fotoPost={'https://picsum.photos/200/152'}
         />
